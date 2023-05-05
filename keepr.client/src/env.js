27ecb1da1369cf6bb18bb674830d86b26e2884db
zroes <zk.roess@gmail.com>
2023-05-05 11:07:04 -0600

    db somewhat setup

--- keepr.client/src/env.js
+++ keepr.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-wrya5phstjdz8awa.us.auth0.com'
+export const clientId = 'AP7043rVAyIAaKzgd3JkL61hWnjf59QY'
+export const audience = 'http://sandbox.xyz'
--- keepr/dbSetup.sql
+++ keepr/dbSetup.sql
@@ -1,8 +1,44 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    keeps(
+        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        creatorId VARCHAR(80) NOT NULL,
+        name VARCHAR(18) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        img VARCHAR(255) NOT NULL,
+        views INT UNSIGNED NOT NULL,
+        Foreign Key (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) DEFAULT CHARSET utf8mb4 COMMENT 'keeps';
+
+DROP TABLE keeps;
+
+CREATE TABLE
+    vaults(
+        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        creatorId VARCHAR(80) NOT NULL,
+        name VARCHAR(18) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        img VARCHAR(255) NOT NULL,
+        isPrivate BOOLEAN NOT NULL DEFAULT false,
+        Foreign Key (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8mb4 comment 'vaults';
+
+CREATE TABLE
+    vaultkeep(
+        id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        creatorId VARCHAR(80) NOT NULL,
+        vaultId INT UNSIGNED NOT NULL,
+        keepId INT UNSIGNED NOT NULL,
+        Foreign Key (vaultId) REFERENCES vaults(id) ON DELETE CASCADE,
+        Foreign Key (keepId) REFERENCES keeps(id) ON DELETE CASCADE,
+        Foreign Key (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) DEFAULT charset utf8mb4 COMMENT 'm2m betwen keeps and values';
